2c663aa8a053c5630429aea4ad62e22533d1218e
IshaanReni <ishaanreni@gmail.com>
2023-03-16 00:55:02 +0000

    Presentable display working with cleaned up design.

--- src/tasks/display.cpp
+++ src/tasks/display.cpp
@@ -24,8 +24,9 @@ void displayUpdateTask(void *pvParameters)
 
     xSemaphoreGive(keyArrayMutex);
 
-    u8g2.clearBuffer();                 // clear the internal memory
-    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    u8g2.clearBuffer(); // clear the internal memory
+    // u8g2.setFont(u8g2_font_ncenB08_tr);       // choose a suitable font
+    u8g2.setFont(u8g2_font_5x7_tr); // choose a suitable font
 
     xSemaphoreTake(queueReceiveMutex, portMAX_DELAY);
 
@@ -33,61 +34,83 @@ void displayUpdateTask(void *pvParameters)
 
     xSemaphoreGive(queueReceiveMutex);
 
-    xSemaphoreTake(currentStepSizesMutex, portMAX_DELAY);
+    // xSemaphoreTake(currentStepSizesMutex, portMAX_DELAY);
 
-    u8g2.setCursor(2, 10);
-    u8g2.print(currentStepSizes[0], HEX);
-    u8g2.setCursor(42, 10);
-    u8g2.print(currentStepSizes[1], HEX);
-    u8g2.setCursor(82, 10);
-    u8g2.print(currentStepSizes[2], HEX);
-    u8g2.setCursor(2, 20);
-    u8g2.print(prevStepSizes[0], HEX);
-    u8g2.setCursor(42, 20);
-    u8g2.print(prevStepSizes[1], HEX);
-    u8g2.setCursor(82, 20);
-    u8g2.print(prevStepSizes[2], HEX);
+    // u8g2.setCursor(2, 10);
+    // u8g2.print(currentStepSizes[0], HEX);
+    // u8g2.setCursor(42, 10);
+    // u8g2.print(currentStepSizes[1], HEX);
+    // u8g2.setCursor(82, 10);
+    // u8g2.print(currentStepSizes[2], HEX);
+    // u8g2.setCursor(2, 20);
+    // u8g2.print(prevStepSizes[0], HEX);
+    // u8g2.setCursor(42, 20);
+    // u8g2.print(prevStepSizes[1], HEX);
+    // u8g2.setCursor(82, 20);
+    // u8g2.print(prevStepSizes[2], HEX);
 
-    xSemaphoreGive(currentStepSizesMutex);
+    // xSemaphoreGive(currentStepSizesMutex);
 
-   u8g2.setCursor(82, 30);
-    uint8_t tmpKnob3Rotation = __atomic_load_n(&knob3Rotation, __ATOMIC_RELAXED);
-    u8g2.println(tmpKnob3Rotation ,HEX);
-
-    u8g2.setCursor(92, 30);
+    u8g2.setCursor(24, 24);
+    u8g2.println("Rev.");
+    u8g2.drawFrame(25, 25, 15, 15);
+    u8g2.setCursor(30, 32);
     uint8_t tmpReverb = __atomic_load_n(&reverb, __ATOMIC_RELAXED);
-    u8g2.println(tmpReverb ,HEX);
+    u8g2.println(tmpReverb, HEX);
 
-    u8g2.setCursor(102, 30);
+    u8g2.setCursor(55, 24);
+    u8g2.println("Pitch");
+    u8g2.drawFrame(60, 25, 15, 15);
+    u8g2.setCursor(65, 32);
     uint8_t tmpKnob2Rotation = __atomic_load_n(&knob2Rotation, __ATOMIC_RELAXED);
     u8g2.println(tmpKnob2Rotation, DEC);
+
+    u8g2.setCursor(95, 24);
+    u8g2.println("Vol");
+    u8g2.drawFrame(95, 25, 15, 15);
+    u8g2.setCursor(100, 32);
+    // u8g2.drawFrame(x, y, size, size);
+    uint8_t tmpKnob3Rotation = __atomic_load_n(&knob3Rotation, __ATOMIC_RELAXED);
+    u8g2.println(tmpKnob3Rotation, HEX);
 
     while (CAN_CheckRXLevel())
       CAN_RX(ID, RX_Message);
 
-    u8g2.setCursor(42, 20);
-    u8g2.setCursor(50, 30);
+    u8g2.setCursor(45, 10);
+    u8g2.println("Octave:");
 
-    u8g2.setCursor(40, 20);
-    if ((westDetect == 0) && (eastDetect == 0))
+    if (tempRX_Message[3] != 0)
     {
-      u8g2.drawStr(80, 20, "Centre");
-    }
-    else if ((westDetect == 0) && (eastDetect == 1))
-    {
-      u8g2.drawStr(80, 20, "Right");
-    }
-    else if ((westDetect == 1) && (eastDetect == 0))
-    {
-      u8g2.drawStr(80, 20, "Left");
+      u8g2.println(tempRX_Message[3]);
     }
     else
     {
-      u8g2.drawStr(80, 20, "-_-");
+      u8g2.println(0);
     }
-    
+
+    u8g2.setCursor(50, 30);
+
+    u8g2.setCursor(40, 20);
+
+    if ((westDetect == 0) && (eastDetect == 0))
+    {
+      u8g2.drawStr(112, 10, "oCo");
+    }
+    else if ((westDetect == 0) && (eastDetect == 1))
+    {
+      u8g2.drawStr(112, 10, "oRo");
+    }
+    else if ((westDetect == 1) && (eastDetect == 0))
+    {
+      u8g2.drawStr(112, 10, "oLo");
+    }
+    else
+    {
+      u8g2.drawStr(112, 10, "-_-");
+    }
+
     // transfer internal memory to the display
-    u8g2.sendBuffer(); 
+    u8g2.sendBuffer();
 
     digitalToggle(LED_BUILTIN); // Toggle LED
   }
